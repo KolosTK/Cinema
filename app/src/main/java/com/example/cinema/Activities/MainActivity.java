3ad953d2c83ff0343f405c03b9dfc00ffcfb3b1d
KolosTK <tanya.kolosenko.1705@gmail.com>
2024-05-12 22:57:14 +0300

    Finish main scrolls

--- app/src/main/java/com/example/cinema/Activities/MainActivity.java
+++ app/src/main/java/com/example/cinema/Activities/MainActivity.java
@@ -16,16 +16,22 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.cinema.Adapters.CategoryListAdapter;
 import com.example.cinema.Adapters.FilmListAdapter;
+import com.example.cinema.Domains.GenreItem;
 import com.example.cinema.Domains.ListFilm;
 import com.example.cinema.R;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
    private  RecyclerView.Adapter adapterNowMovies,adapterUpcomming,adapterCategory;
    private RecyclerView recyclerViewNowMovies, recyclerViewUpcomming,recyclerViewCategory;
    private RequestQueue mRequestQueue;
-   private StringRequest mStringRequest, mStringRequest2,getmStringRequest3;
+   private StringRequest mStringRequest, mStringRequest2, mStringRequest3;
    private ProgressBar loading1,loading2,loading3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +40,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         
         initView();
-        sendRequest();
+        sendRequestNowInCinema();
+        sendRequestCategory();
+        sendRequestUpcomming();
     }
 
-    private void sendRequest() {
+    private void sendRequestNowInCinema() {
         mRequestQueue = Volley.newRequestQueue(this);
         loading1.setVisibility(View.VISIBLE);
-        mStringRequest = new StringRequest(Request.Method.GET, "https://moviesapi.ir/api/v1/movies?page=2", new Response.Listener<String>() {
+        mStringRequest = new StringRequest(Request.Method.GET, "https://moviesapi.ir/api/v1/movies?page=3", new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 Gson gson = new Gson();
@@ -59,12 +67,57 @@ public class MainActivity extends AppCompatActivity {
         mRequestQueue.add(mStringRequest);
     }
 
+    private void sendRequestUpcomming() {
+        mRequestQueue = Volley.newRequestQueue(this);
+        loading3.setVisibility(View.VISIBLE);
+        mStringRequest3 = new StringRequest(Request.Method.GET, "https://moviesapi.ir/api/v1/movies?page=4", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Gson gson = new Gson();
+                loading3.setVisibility(View.GONE);
+                ListFilm items = gson.fromJson(response, ListFilm.class);
+                adapterUpcomming = new FilmListAdapter(items);
+                recyclerViewUpcomming.setAdapter(adapterUpcomming);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                loading3.setVisibility(View.GONE);
+                Log.i("UiLover", "onErrorResponse:" + error.toString());
+            }
+        });
+        mRequestQueue.add(mStringRequest3);
+    }
+
+    private void sendRequestCategory() {
+        mRequestQueue = Volley.newRequestQueue(this);
+        loading2.setVisibility(View.VISIBLE);
+        mStringRequest2 = new StringRequest(Request.Method.GET, "https://moviesapi.ir/api/v1/genres", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Gson gson = new Gson();
+                loading2.setVisibility(View.GONE);
+                ArrayList<GenreItem> categoryList = gson.fromJson(response,new TypeToken<ArrayList<GenreItem>>(){
+                }.getType());
+                adapterCategory = new CategoryListAdapter(categoryList);
+                recyclerViewCategory.setAdapter(adapterCategory);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                loading2.setVisibility(View.GONE);
+                Log.i("UiLover", "onErrorResponse:" + error.toString());
+            }
+        });
+        mRequestQueue.add(mStringRequest2);
+    }
+
     private void initView(){
         recyclerViewNowMovies=findViewById(R.id.recyclerView);
         recyclerViewNowMovies.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
-        recyclerViewUpcomming=findViewById(R.id.recyclerView2);
+        recyclerViewUpcomming=findViewById(R.id.recyclerView3);
         recyclerViewUpcomming.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
-        recyclerViewCategory=findViewById(R.id.recyclerView3);
+        recyclerViewCategory=findViewById(R.id.recyclerView2);
         recyclerViewCategory.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
 
         loading1=findViewById(R.id.progressBar1);
---
+++ app/src/main/java/com/example/cinema/Domains/GenreItem.java
@@ -0,0 +1,22 @@
+package com.example.cinema.Domains;
+
+public class GenreItem {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    private String name;
+    private Integer id;
+}
