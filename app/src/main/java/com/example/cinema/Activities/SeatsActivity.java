7cc9bc5acde3df0b0073f436622200c87cbe9ca9
KolosTK <tanya.kolosenko.1705@gmail.com>
2024-06-08 13:28:06 +0300

    Implement seats logic
    Count amount of selected seats and cost

--- app/src/main/java/com/example/cinema/Activities/SeatsActivity.java
+++ app/src/main/java/com/example/cinema/Activities/SeatsActivity.java
@@ -1,28 +1,63 @@
 package com.example.cinema.Activities;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.cinema.R;
 
 public class SeatsActivity extends AppCompatActivity {
     private ImageView backButton;
+    private Button button1,button2,button3,button4,button5,button6,button7,button8,button9,button10,button11,button12,button13,button14;
+    private TextView seatsAmount, costAmount;
+    private int seatsCount,totalCost = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_seats);
-//        idFilm=getIntent().getIntExtra("id",0);
+
+        setupButton(findViewById(R.id.button1), 100);
+        setupButton(findViewById(R.id.button2), 100);
+        setupButton(findViewById(R.id.button3), 100);
+        setupButton(findViewById(R.id.button4), 100);
+        setupButton(findViewById(R.id.button5), 100);
+        setupButton(findViewById(R.id.button6), 100);
+        setupButton(findViewById(R.id.button7), 100);
+        setupButton(findViewById(R.id.button8), 100);
+        setupButton(findViewById(R.id.button9), 100);
+        setupButton(findViewById(R.id.button10), 100);
+        setupButton(findViewById(R.id.button11), 100);
+        setupButton(findViewById(R.id.button12), 100);
+        setupButton(findViewById(R.id.button13), 100);
+        setupButton(findViewById(R.id.button14), 100);
+
+        seatsAmount = findViewById(R.id.seatsAmount);
+        costAmount = findViewById(R.id.costAmount);
+
         initView();
-//        sendRequest();
     }
+    private void setupButton(Button button, int seatCost) {
+        button.setOnClickListener(view -> {
+            button.setSelected(!button.isSelected()); // Toggle selection state
+            if (button.isSelected()) {
+                button.setBackground(ContextCompat.getDrawable(SeatsActivity.this, R.drawable.button_background_empty));
+                seatsCount++;
+                totalCost += seatCost;
+            } else {
+                button.setBackground(ContextCompat.getDrawable(SeatsActivity.this,  R.drawable.button_background));
+                seatsCount--;
+                totalCost -= seatCost;
+            }
+            seatsAmount.setText(String.valueOf(seatsCount));
+            costAmount.setText(String.valueOf(totalCost));
+        });
+    }
+
     private void initView() {
         backButton = findViewById(R.id.arrow);
         backButton.setOnClickListener(v -> finish());
